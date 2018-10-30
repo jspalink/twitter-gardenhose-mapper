b1ceeb5fb974a9d9412f4409275aac83903676f6
Jonathan Spalink <spalink@gmail.com>
2018-10-30 13:31:05 -0500

    minor

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     author='Jonathan Spalink',
     author_email='jspalink@econtext.ai',
     description='Filter the twitter stream, and simply run it through the eContext API in order to publish content to eContext Kafka Stream',
-    install_requires=['tweepy', 'econtextapi'],
+    install_requires=['tweepy', 'econtext.util'],
+    dependency_links=[
+        'git+ssh://git@github.com/info-com/econtext.util#egg=econtext.util-1.0.3'
+    ],
     
     entry_points={
         'console_scripts': [
